422e1cc8e1b72e8224cbb57e11331d5b74ca0841
Morne Erasmus <morne.erasmus@digitalskillsacademy.me>
2017-01-04 20:36:28 +0200

    got it working

--- Controllers/TaskITController.js
+++ Controllers/TaskITController.js
@@ -3,6 +3,12 @@
 var TaskITApp = angular.module('TaskITApp', []);
 
 TaskITApp.controller('TaskITController',function($scope){
-    $scope.lol = 5;
+    
+    $scope.todoList = [{taskTitle:'Develop App',taskDescr:'Research AngularJS and GIT',status:'true'},{taskTitle:'Do something else',taskDescr:'Stop sitting infront of computer',status:'false'}];
+    $scope.totalItems = $scope.todoList.length;
+    
+     angular.forEach($scope.todoList, function(value) {
+       value.status);
+     });
 });
   
