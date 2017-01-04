f9f96e0db6f5cdf7fcf24f514c5d635193a4623b
Morne Erasmus <morne.erasmus@digitalskillsacademy.me>
2017-01-04 22:13:22 +0200

    push and update done
    Managed to push items to array and also display currently outstanding
    tasks.

--- Controllers/TaskITController.js
+++ Controllers/TaskITController.js
@@ -1,14 +1,49 @@
 
-  
 var TaskITApp = angular.module('TaskITApp', []);
 
 TaskITApp.controller('TaskITController',function($scope){
     
-    $scope.todoList = [{taskTitle:'Develop App',taskDescr:'Research AngularJS and GIT',status:'true'},{taskTitle:'Do something else',taskDescr:'Stop sitting infront of computer',status:'false'}];
-    $scope.totalItems = $scope.todoList.length;
     
-     angular.forEach($scope.todoList, function(value) {
-       value.status);
-     });
+    
+    $scope.todoList = [
+        {taskTitle:'Develop App',taskDescr:'Research AngularJS and GIT',status:true},
+        {taskTitle:'Do something else',taskDescr:'Stop sitting infront of computer',status:false}
+    ];
+ 
+     $scope.addTask = function () {
+        $scope.todoList.push({taskTitle:$scope.taskItemTitle,taskDescr:$scope.taskItemDescr,status:false});
+        $scope.taskItemTitle = "";
+        $scope.taskItemDescr = "";
+         
+    };
+    
+    
+    angular.forEach($scope.todoList, function(x) {
+        if(x.status == false) {
+            $scope.outstandingTasks += 1;
+        } 
+    });
+
+    
+    $scope.outstandingList = function () {
+       var outstandingTasks = 0;
+       $scope.totalItems = 0;
+        angular.forEach($scope.todoList,function(todoItem){
+            $scope.totalItems += 1;
+            if(todoItem.status == false){
+                 outstandingTasks += 1;
+            }
+        });
+        return outstandingTasks;
+    }
+    
+    
+    $scope.removeTask = function () {
+              
+    };
+
+    
+    
+    
 });
   
