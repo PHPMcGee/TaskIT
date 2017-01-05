d88fb10d2f835ba6ce9c92b53c84baf92938bed4
Morne Erasmus <morne.erasmus@digitalskillsacademy.me>
2017-01-05 10:00:06 +0200

    Working 100%

--- Controllers/TaskITController.js
+++ Controllers/TaskITController.js
@@ -3,16 +3,20 @@ var TaskITApp = angular.module('TaskITApp', []);
 
 TaskITApp.controller('TaskITController',function($scope){
     
-    var taskNmbr = 2;
+    //$scope.totalLength = $scope.todoList.length;
     
     $scope.todoList = [
-        {taskNumber:1,taskTitle:'Develop App',taskDescr:'Research AngularJS and GIT',status:true},
-        {taskNumber:2,taskTitle:'Do something else',taskDescr:'Stop sitting infront of computer',status:false}
+        {taskID:'task1',taskTitle:'Develop App',taskDescr:'Research AngularJS and GIT',status:true},
+        {taskID:'task2',taskTitle:'Do something else',taskDescr:'Stop sitting infront of computer',status:false}
     ];
+    
+    
  
      $scope.addTask = function () {
-        taskNmbr += 1;
-        $scope.todoList.push({taskNumber:taskNmbr,taskTitle:$scope.taskItemTitle,taskDescr:$scope.taskItemDescr,status:false});
+        var num = 0;
+        var taskUnique = "";
+        taskUnique = "task" + ($scope.todoList.length + 1); 
+        $scope.todoList.push({taskID:taskUnique,taskTitle:$scope.taskItemTitle,taskDescr:$scope.taskItemDescr,status:false});
         $scope.taskItemTitle = "";
         $scope.taskItemDescr = "";
          
@@ -39,8 +43,21 @@ TaskITApp.controller('TaskITController',function($scope){
     }
     
     
-    $scope.removeTask = function (tester) {
-             alert(tester);
+    $scope.removeTask = function (taskNum) {
+        var findIndex = -1;
+        var found = 0;
+        angular.forEach($scope.todoList,function(itm){
+           findIndex += 1;
+           if(itm.taskID == taskNum) {
+               found = findIndex;
+           } 
+        });
+      //  alert(found);
+        $scope.todoList.splice(found,1);
+    };
+    
+    $scope.clearTasks = function () {
+        $scope.todoList = [];
     };
 
     
